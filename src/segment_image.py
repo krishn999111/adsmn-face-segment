c86965265cf970288e7ad5f33dfc0495affecb95
krishn999111 <163983905+krishn999111@users.noreply.github.com>
2024-03-19 13:56:45 +0530

    Update segment_image.py

--- src/segment_image.py
+++ src/segment_image.py
@@ -68,19 +68,18 @@ def segment_image(url, processor, model):
     included_classes_mask = torch.zeros_like(pred_seg)
     for class_idx in included_class_indices:
         included_classes_mask[pred_seg == class_idx] = 1
-
+    
     included_classes_mask_np = included_classes_mask.numpy().astype(np.uint8)
+    output_image = np.array(image.convert("RGBA"))    
+    output_image[..., 3][included_classes_mask_np == 0] = 0
     
-    output_image = np.array(image)
-    output_image[included_classes_mask_np == 0] = 255
+    output_image_path = "segmented_image_face.png"
     
-    output_image_path = "segmented_image_face.jpg"
-
     output_image_pil = Image.fromarray(output_image)
-
+    
     if os.path.exists(output_image_path):
         os.remove(output_image_path)
-
+    
     output_image_pil.save(output_image_path)
     
     return output_image_path
