90f99adb798fb3961615d35b4da2cd57107b0d3a
krishn999111 <163983905+krishn999111@users.noreply.github.com>
2024-03-19 13:56:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import segmented_image_face from './segmented_image_face.jpg';
+import segmented_image_face from './segmented_image_face.png';
 import './App.css';
 
 function App() {
